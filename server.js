fff82d556fef612d9a8699e725facd2e6b5f9eea
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-06-07 11:31:54 +0545

    Update server.js

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ if(process.env.NODE_ENV ==='production'){
 }
 
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 8080;
 app.listen(PORT,()=> console.log(`server is started on ${PORT}`))
